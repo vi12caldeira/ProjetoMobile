fc1e1f2e42c89795e28de84d2fd9f8a4759a703e
igorasr <igoraugustosouza00@gmail.com>
2022-11-15 18:08:49 -0300

    Login page view

--- projeto_cliente/lib/routes.dart
+++ projeto_cliente/lib/routes.dart
@@ -1,17 +1,17 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
+import 'package:navegacao_drawer/pages/login_page.dart';
 
-import 'pages/cupons_page.dart';
 import 'pages/cartoes_page.dart';
-import 'pages/pedidos_page.dart';
+import 'pages/cupons_page.dart';
 import 'pages/home_page.dart';
+import 'pages/pedidos_page.dart';
 
-Map<String, WidgetBuilder> routes(){
-  return{
-    '/': (context) => HomePage(),
+Map<String, WidgetBuilder> routes() {
+  return {
+    '/': (context) => LoginPage(),
+    '/home': (context) => HomePage(),
     '/cupons': (context) => CuponsPage(),
     '/cartoes': (context) => CartoesPage(),
-    '/pedidos': (context) =>PedidosPage(),
+    '/pedidos': (context) => PedidosPage(),
   };
 }
