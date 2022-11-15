0af9f713ef587b636ec16f9be8cc9ed63acd98fe
VitoriaMarcia2 <marciavitoria471@gmail.com>
2022-11-15 19:02:30 -0300

    hshs

--- projeto_loja/lib/pages/home_page.dart
+++ projeto_loja/lib/pages/home_page.dart
@@ -6,63 +6,62 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Distribuidora'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            //Image.asset('assets/refrigerante.png'),
-          ],
+        appBar: AppBar(
+          title: const Text('Distribuidora'),
+          centerTitle: true,
         ),
-        ),
-
-      drawer: SafeArea(
-        child: Drawer(
-          child: ListView(
-            children: [
-              const UserAccountsDrawerHeader(
-                accountName: Text('Seu nome e sobrenome'),
-                accountEmail: Text('seuemail@mail.com'),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: AssetImage('assets/images/bart.jpg'),
-                ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.receipt_long),
-                title: const Text("Pedidos"),
-                subtitle: const Text("mais informações..."),
-                trailing: const Icon(Icons.arrow_forward),
-                onTap: () {Navigator.pop(context);
-                  Navigator.pushNamed(context, "/pedidos");},
-              ),
-              ListTile(
-                leading: const Icon(Icons.add),
-                title: const Text("Cadastrar Produtos"),
-                subtitle: const Text("mais informações..."),
-                trailing: const Icon(Icons.arrow_forward),
-                onTap: () {Navigator.pop(context);
-                  Navigator.pushNamed(context, "/cadastrarprodutos");},
-              ),
-              ListTile(
-                leading: const Icon(Icons.library_add),
-                title: const Text("Cadastrar loja"),
-                subtitle: const Text("mais informações..."),
-                trailing: const Icon(Icons.arrow_forward),
-                onTap: () {Navigator.pop(context);
-                  Navigator.pushNamed(context, "/cadastrarloja");},
-              ),
-              ListTile(
-                leading: const Icon(Icons.exit_to_app),
-                title: const Text("Logout"),
-                trailing: const Icon(Icons.arrow_forward),
-                onTap: () {print("Logout");},
-              ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              //Image.asset('assets/refrigerante.png'),
             ],
           ),
         ),
-      ),
+        drawer: SafeArea(
+    child: Drawer(
+    child: ListView(
+    children: [
+    const UserAccountsDrawerHeader(
+    accountName: Text('Seu nome e sobrenome'),
+    accountEmail: Text('seuemail@mail.com'),
+    currentAccountPicture: CircleAvatar(
+    backgroundImage: AssetImage('assets/images/bart.jpg'),
+    ),
+    ),
+    ListTile(
+    leading: const Icon(Icons.receipt_long),
+    title: const Text("Pedidos"),
+    subtitle: const Text("mais informações..."),
+    trailing: const Icon(Icons.arrow_forward),
+    onTap: () {Navigator.pop(context);
+    Navigator.pushNamed(context, "/pedidos");},
+    ),
+    ListTile(
+    leading: const Icon(Icons.add),
+    title: const Text("Cadastrar Produtos"),
+    subtitle: const Text("mais informações..."),
+    trailing: const Icon(Icons.arrow_forward),
+    onTap: () {Navigator.pop(context);
+    Navigator.pushNamed(context, "/cadastrarprodutos");},
+    ),
+    ListTile(
+    leading: const Icon(Icons.library_add),
+    title: const Text("Cadastrar loja"),
+    subtitle: const Text("mais informações..."),
+    trailing: const Icon(Icons.arrow_forward),
+    onTap: () {Navigator.pop(context);
+    Navigator.pushNamed(context, "/cadastrarloja");},
+    ),
+    ListTile(
+    leading: const Icon(Icons.exit_to_app),
+    title: const Text("Logout"),
+    trailing: const Icon(Icons.arrow_forward),
+    onTap: () {print("Logout");},
+    ),
+    ],
+    ),
+    ),
+    ),
 
     );
   }
